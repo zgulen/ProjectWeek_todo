fbdb4da290015de2bdd5ea672f3ba65d505e8383
zgulen <gulenzubeyir@gmail.com>
2022-06-11 17:07:37 +0300

    confusing

--- local.js
+++ local.js
@@ -1,74 +1,54 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+//? Selectors
 const btn = document.getElementById('todo-button');
 const todoInput = document.getElementById('todo-input');
 const todoUl = document.getElementById('todo-ul');
 
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
-renderSavedTodos();
-function renderSavedTodos(){
-    todos.forEach((todo) => {
-        const {id , isDown, content} = todo
-        todoUl.innerHTML += `
-    <li>
-      <i class="fa fa-check"></i>
-      <p>${content}</p>
-      <i class="fa fa-trash"></i>
-    </li>`;
-    });
-}
-
+//? Baslangicta input aktif olsun
 window.onload = function () {
   todoInput.focus();
 };
 
+//? Add Buton Event'inin tanimanmasi
 btn.addEventListener('click', (e) => {
-  console.log(todoInput.value);
-  if(todoInput.value){
-    const todoObject = {
-        id: new Date().getTime(),
-        isDown: false,
-        content: todoInput.value
-    }
-
-    todos.push(todoObject)
-    
-    localStorage.setItem("todos", JSON.stringify(todos))
-    
+  if (!todoInput.value) {
+    alert('Please enter your todo');
+  } else {
     todoUl.innerHTML += `
     <li>
       <i class="fa fa-check"></i>
       <p>${todoInput.value}</p>
       <i class="fa fa-trash"></i>
     </li>`;
-  todoInput.value = '';
+    todoInput.value = '';
   }
 });
 
+//? Klavyeden enter tusuna basilmasi ile add butonunun click fonksiyonunun cagrilmasi
 todoInput.addEventListener('keydown', (e) => {
-    if(todoInput.value){
-        if (e.code === 'Enter') {
+  if (e.key === 'Enter') {
     btn.click();
   }
-    }
 });
 
-todoUl.addEventListener("click" , (e)=>{
-    if(e.target.classList.contains("fa-trash")){
-        e.target.parentElement.remove()
-    }
-})
+//? delete ve ok butonları için event tanimlamasi
+todoUl.addEventListener('click', (e) => {
+  //!Event, sil butonlarının birisinden geldi ise
+  if (e.target.classList.contains('fa-trash')) {
+    //? ilgili li elementini silerek DOM'u güncelle
+    e.target.parentElement.remove();
+  }
 
-todoUl.addEventListener("click" , (e)=>{
-    if(e.target.classList.contains("fa-check")){
-        if(e.target.parentElement.classList.contains("checked")){
-            e.target.parentElement.classList.remove("checked")
-        }else{
-            e.target.parentElement.classList.add("checked")
-        }
-        // let x = document.querySelector("li")
-        // x.className = "checked"
+  //!Event, silme veya ok butonlarından geldi ise
+  if (e.target.classList.contains('fa-check')) {
+    //? ilgili li elementinde checked adinda bir class varsa bunu sil
+    if (e.target.parentElement.classList.contains('checked')) {
+      e.target.parentElement.classList.remove('checked');
+    } else {
+      //? ilgili li elementinde checked adinda bir class yoksa ekle
+      e.target.parentElement.classList.add('checked');
     }
-})
+  }
+});
